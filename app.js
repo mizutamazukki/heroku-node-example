10a0c23ac85ccc7842d22595779704dd7b64aee1
樋浦 <mizutama12120329@gmail.com>
2021-12-17 23:29:05 +0900

    result

--- app.js
+++ app.js
@@ -3,19 +3,6 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 5000;
 
-// レスポンスのデータ
-const responseObjectData = {
-  textObject1 : {
-    title: 'Objectのタイトル1です',
-    subTitle: 'Objectのサブタイトル1です',
-    bodyText: 'Objectの本文1です'
-  },
-  textObject2 : {
-    title: 'Objectのタイトル2です',
-    subTitle: 'Objectのサブタイトル2です',
-    bodyText: 'Objectの本文2です'
-  },
-};
 var app = express();
 app.get("/", (req, res) => {
   res.status(200).send("Express!!");
@@ -23,6 +10,10 @@ app.get("/", (req, res) => {
 app.get("/api/news", function(req, res, next) {
   // ここに処理を書く
 res.json(JSON.parse(fs.readFileSync('./api/news.json', 'utf8')));
+});
+app.get("/api/result", function(req, res, next) {
+  // ここに処理を書く
+res.json(JSON.parse(fs.readFileSync('./api/result.json', 'utf8')));
 });
 // ポート3000番でlistenする
 app.listen(PORT);
